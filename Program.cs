0bc49217088c80c7ed49ee1b0c82791c48ffb654
Wallyson Gomes <63168683+fuzaros@users.noreply.github.com>
2026-01-18 11:56:12 -0300

    implementado dos os TODO's das tarefas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "IMEI123456", 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Iphone iphone = new Iphone("987654321", "iPhone 12", "IMEI987654", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
